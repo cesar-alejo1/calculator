dcd43c3324a407e0b4f2fafd02887e8ce04fabca
cesar-alejo1 <cesaralejo.dev@gmail.com>
2025-04-30 13:18:57 -0500

    Add operations mul. and div.

--- src/calculator.js
+++ src/calculator.js
@@ -5,3 +5,12 @@ function sumar(a, b) {
 function restar(a, b) {
   return a - b;
 }
+
+function multiplicar(a, b) {
+  return a * b;
+}
+
+function dividir(a, b) {
+  if (b === 0) throw new Error("Divition by 0 not permit.")
+  return a / b;
+}
